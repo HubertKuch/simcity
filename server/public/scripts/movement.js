bbda9d27858ce3357e835c7f50e2b122f2afad41
HubertKuch <kuchhhubert@gmail.com>
2021-12-31 10:11:02 +0100

    Change folder structure

--- server/public/scripts/movement.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-function dragMovement (e) {
-    console.log(e.x);
-}
-
-document.addEventListener('touchstart', dragMovement);
--- server/utils/sendStatus.ts
+++
@@ -1,5 +0,0 @@
-import {Response} from "express";
-
-export default function sendStatus (res: Response, message: string, statusCode: number, status: string, data: {} = {} ) {
-    return res.status(statusCode).json({ message, statusCode, status, data });
-}
