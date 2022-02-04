13667d680738fe6c53d5d216c34e84b2805b878b
HubertKuch <kuchhhubert@gmail.com>
2022-02-04 17:48:52 +0100

    Change CSP headers policy

--- middleware/headers.ts
+++ middleware/headers.ts
@@ -3,7 +3,7 @@ import { Response, Request, NextFunction } from 'express';
 export default (req: Request, res: Response, next: NextFunction) => {
     res.setHeader(
         'Content-Security-Policy',
-        "default-src *; font-src *;"
+        "default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline';"
       );
 
     next();
