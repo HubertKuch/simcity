b502dc907d4af195db4961c3de8bf7b2dd794c30
HubertKuch <kuchhhubert@gmail.com>
2022-02-04 17:49:36 +0100

    Add pre movement functionality

---
+++ public/movement.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/*
+let x = 0;
+let y = 0;
+let isDown = false;
+
+[...$$('.chunk')].forEach(chunk => {
+    chunk.addEventListener('drag', (e) => {
+        e.preventDefault();
+    });
+})
+
+map.addEventListener('mousedown', (ev) => {
+    isDown = true;
+    console.log("DOWN \n",ev)
+}, true);
+
+map.addEventListener('mouseup', (ev) => {
+    isDown = false;
+    console.log("UP \n", ev)
+}, true);
+
+*/
