31df1c266444923ec22aae1608932c29ad422665
HubertKuch <kuchhhubert@gmail.com>
2022-02-04 17:47:45 +0100

    Add user city info interface

---
+++ models/Info.ts
@@ -0,0 +1,11 @@
+export default interface Info {
+    money: number;
+    level: number;
+    exp: number;
+    earnings: number;
+    expenses: number;
+    producedEnergy:number;
+    usedEnergy: number;
+    buildings: number;
+    people: number;
+}
